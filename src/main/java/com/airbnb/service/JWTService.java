22f60d418a50e591b900ee616f4509f7324145e4
unknown <aayushsingh050801@gmail.com>
2024-04-19 13:19:49 +0530

    adding JWT token

---
+++ src/main/java/com/airbnb/service/JWTService.java
@@ -0,0 +1,38 @@
+package com.airbnb.service;
+
+import com.airbnb.entity.PropertyUser;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JWTService {
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
+    @Value("${jwt.issuer}")
+    private String issuer;
+    @Value("${jwt.expiry.duration}")
+    private int expiryTime;
+    private Algorithm algorithm;
+
+    private final static String USER_NAME="username";
+    @PostConstruct
+    public void postConstruct(){
+        algorithm=Algorithm.HMAC256(algorithmKey);
+        /**
+         * Algorithm is Static Variable. So, access using class name.
+         */
+    }
+
+    public String generateToken(PropertyUser propertyUser){
+        return JWT.create().
+                withClaim(USER_NAME, propertyUser.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expiryTime))
+                .withIssuer(issuer)
+                .sign(algorithm);
+    }
+}
